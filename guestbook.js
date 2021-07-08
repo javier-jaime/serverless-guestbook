fd1d62e015ea1edcce0972ae52495dd077460359
javier-jaime <68567459+javier-jaime@users.noreply.github.com>
2021-07-08 09:20:04 -0600

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://1691850f.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
